a5cf9ec3cf7233106e5098c7ee2c6196f3dcb42b
junchen.zhou <junchen.zhou@mihoyo.com>
2018-12-17 17:31:11 +0800

    Add pico8.workspace configuration

--- src/extension.ts
+++ src/extension.ts
@@ -9,13 +9,12 @@ export function activate(context: vscode.ExtensionContext) {
     let p8Config = vscode.workspace.getConfiguration('pico8');
 
     let disposable = vscode.commands.registerTextEditorCommand('pico8.run', (textEditor: vscode.TextEditor) => {
-
         let fileName = textEditor.document.fileName;
         let args = ["-windowed", "1", "-run", fileName];
+        let workspacePath = p8Config['workspacePath'];
         
-        let workspace = vscode.workspace;
-        if (workspace) {
-            args.push("-home", workspace.rootPath);
+        if (workspacePath.length > 0) {
+            args.push("-home", workspacePath);
         }
 
         cp.execFile(p8Config['executablePath'], args, { env: process.env }, (err, stdout, stderr) => {
